9c5bcb48a040b6a5794d6221d2b0654d7ae39666
berwizlelanzy <bernadusindrawijaya@gmail.com>
2022-11-27 19:54:57 +0700

    Mobil city b 1

---
+++ D-09off/ScriptMobil/CarB.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _process(delta):
+	var y = position[1]
+	if (y >= 700):
+		queue_free()
+	else:
+		translate(Vector2(0, 1))
+
+func _on_Mobil_body_entered(body):
+	get_node("../CanvasLayer").show()
+	get_node("../CanvasLayer/GameOver").get_child(2).play()
+	get_node("../TileMap/building/KinematicBody2D").isDead = true
---
+++ D-09off/ScriptMobil/MobilCityB.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+func _ready():
+	var rand = RandomNumberGenerator.new()
+	
+	yield(get_tree().create_timer(2.0), "timeout") #kasi delay 2 detik sebelum mulai spawn mobil
+	while true:
+		var mobil = load("res://CarB.tscn").instance()
+		mobil.set_position(Vector2(96, -48))
+		mobil.set_rotation(PI)
+		var texture = rand.randi_range(0, 2) # 0 > red, 1 > green, 2 > blue
+		match (texture):
+			0:
+				mobil.get_node("Mobil/Sprite").texture = load("res://AssetMap/redCar.png")
+			1:
+				mobil.get_node("Mobil/Sprite").texture = load("res://AssetMap/greenCar.png")
+			2:
+				mobil.get_node("Mobil/Sprite").texture = load("res://AssetMap/blueCar.png")
+		
+		add_child(mobil)
+		
+		var mobil2 = load("res://CarB2.tscn").instance()
+		mobil2.set_position(Vector2(128, 700))
+		texture = rand.randi_range(0, 2) # 0 > red, 1 > green, 2 > blue
+		match (texture):
+			0:
+				mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/redCar.png")
+			1:
+				mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/greenCar.png")
+			2:
+				mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/blueCar.png")
+		
+		add_child(mobil2)
+		yield(get_tree().create_timer(2.0), "timeout")
