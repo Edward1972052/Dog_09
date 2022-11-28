fd4343a4c38feb2fe02712ef743110e53b3a2615
berwizlelanzy <bernadusindrawijaya@gmail.com>
2022-11-28 11:47:01 +0700

    Mobil City A 2

--- D-09off/ScriptMobil/MobilCityA.gd
+++ D-09off/ScriptMobil/MobilCityA.gd
@@ -18,16 +18,16 @@ func _ready():
 		
 		add_child(mobil)
 		
-		#var mobil2 = load("res://CarB2.tscn").instance()
-		#mobil2.set_position(Vector2(128, 700))
-		#texture = rand.randi_range(0, 2) # 0 > red, 1 > green, 2 > blue
-		#match (texture):
-		#	0:
-		#		mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/redCar.png")
-		#	1:
-		#		mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/greenCar.png")
-		#	2:
-		#		mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/blueCar.png")
-		#
-		#add_child(mobil2)
+		var mobil2 = load("res://CarA2.tscn").instance()
+		mobil2.set_position(Vector2(1128, 288))
+		texture = rand.randi_range(0, 2) # 0 > red, 1 > green, 2 > blue
+		match (texture):
+			0:
+				mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/redCar.png")
+			1:
+				mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/greenCar.png")
+			2:
+				mobil2.get_node("Mobil/Sprite").texture = load("res://AssetMap/blueCar.png")
+		
+		add_child(mobil2)
 		yield(get_tree().create_timer(2.0), "timeout")
