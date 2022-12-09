75ad5e55552ce676cf5d34415650ff07fc408ea6
berwizlelanzy <bernadusindrawijaya@gmail.com>
2022-12-09 14:13:55 +0700

    Scene change transition

--- D-09off/Mapper.gd
+++ D-09off/Mapper.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
-
+func _ready():
+	get_node("../Transition").play("fade_out")
 
 func _on_Area2D_body_entered(_body):
+	get_node("../Transition").play("fade_to_black")
+	yield(get_tree().create_timer(0.6), "timeout")
 	get_tree().change_scene("res://"+ self.name +".tscn")
-	
